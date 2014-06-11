fd054e99ad4041d7ba255a2890f6144d9e6b0970
facumolina <facu.molina7@gmail.com>
2014-06-11 12:05:55 -0300

    Correcion eliminación

--- appCompraVenta/src/Eliminacion.java
+++ appCompraVenta/src/Eliminacion.java
@@ -2,6 +2,7 @@
  * Clase que implementa métodos para efectuar las elimaciones que se deseen sobre la base de datos del sistema.
  * @authors Angelo Chávez - Eduardo Benmergui - Facundo Molina 
  */
+import java.util.Date;
 import java.sql.*;
 
 public class Eliminacion {
@@ -13,14 +14,14 @@ public class Eliminacion {
 	public static void eliminarAnuncio(int id){
 		try {
 			Statement statement = Conexion.getConexion().createStatement();
-		
-			String query ="DELETE FROM Anuncio WHERE id="+id+";";
+			String query ="UPDATE Anuncio SET fechaBaja=NOW() WHERE id="+id+";";
 			statement.execute(query);
 		
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 			System.err.println("Error connecting: " + sqle);
 		}
+		
 	}
 	
 	
